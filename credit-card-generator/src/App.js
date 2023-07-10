7e8aff720f53fde2836a9c1b85f5b931e7dd032f
priyanshuharshbodhi1 <priyanshuqpwp@gmail.com>
2023-07-10 18:09:30 +0530

    made simple prototype of the card component

--- credit-card-generator/src/App.js
+++ credit-card-generator/src/App.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import BgMainDesktop from './images/bg-main-desktop.png';
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className="container">
+      <div className="image-container">
+        <img src={BgMainDesktop} alt="" />
+      </div>
+      <div className="form-container">
+        <form>
+          <div className="form-row">
+            <label htmlFor="cardholder-name">Cardholder Name</label>
+            <input type="text" id="cardholder-name" />
+          </div>
+          <div className="form-row">
+            <label htmlFor="card-number">Card Number</label>
+            <input type="text" id="card-number" />
+          </div>
+          <div className="form-row">
+            <div className="form-column">
+              <label htmlFor="expiry-date">Expiry Date</label>
+              <input type="text" id="expiry-date" />
+            </div>
+            <div className="form-column">
+              <label htmlFor="cvc">CVC</label>
+              <input type="text" id="cvc" />
+            </div>
+          </div>
+          <div className="form-row">
+            <button type="submit">Confirm</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
